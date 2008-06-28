d38796d444640510775ca1c69f865b64b9975c24
Dr Nic Williams <drnicwilliams@gmail.com>
2008-06-28 23:58:42 +1000

    model: test file includes a default test to check the bundle integration is working

--- iphoneruby_generators/model/templates/test.rb
+++ iphoneruby_generators/model/templates/test.rb
@@ -4,4 +4,7 @@ require "<%= class_name %>.bundle"
 OSX::ns_import :<%= class_name %>
 
 class Test<%= class_name %> < Test::Unit::TestCase
+  def test_<%= name %>_class_exists
+    OSX::<%= class_name %>
+  end
 end
