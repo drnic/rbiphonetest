68e784fa92009a6e5f37faff3c84a6bc21843c0e
Dr Nic Williams <drnicwilliams@gmail.com>
2008-06-28 23:22:19 +1000

    initial commit; with app generator

---
+++ app_generators/iphoneruby/iphoneruby_generator.rb
@@ -0,0 +1,68 @@
+class IphonerubyGenerator < RubiGen::Base
+
+  DEFAULT_SHEBANG = File.join(Config::CONFIG['bindir'],
+                              Config::CONFIG['ruby_install_name'])
+
+  default_options :author => nil
+
+  attr_reader :name
+
+  def initialize(runtime_args, runtime_options = {})
+    super
+    usage if args.empty?
+    @destination_root = File.expand_path(args.shift)
+    @name = base_name
+    extract_options
+  end
+
+  def manifest
+    record do |m|
+      # Ensure appropriate folder(s) exists
+      m.directory ''
+      BASEDIRS.each { |path| m.directory path }
+
+      # Create stubs
+      m.file_copy_each ["Rakefile"]
+      m.file           "dot_autotest", ".autotest"
+      m.file_copy_each ["test/test_helper.rb"]
+
+      m.dependency "install_rubigen_scripts", [destination_root, 'iphoneruby'],
+        :shebang => options[:shebang], :collision => :force
+    end
+  end
+
+  protected
+    def banner
+      <<-EOS
+Want to write unit tests in Ruby? Run this generator
+in the root folder of your Xcode project for your iPhone app.
+
+USAGE: #{spec.name} name
+EOS
+    end
+
+    def add_options!(opts)
+      opts.separator ''
+      opts.separator 'Options:'
+      # For each option below, place the default
+      # at the top of the file next to "default_options"
+      # opts.on("-a", "--author=\"Your Name\"", String,
+      #         "Some comment about this option",
+      #         "Default: none") { |options[:author]| }
+      opts.on("-v", "--version", "Show the #{File.basename($0)} version number and quit.")
+    end
+
+    def extract_options
+      # for each option, extract it into a local variable (and create an "attr_reader :author" at the top)
+      # Templates can access these value via the attr_reader-generated methods, but not the
+      # raw instance variable value.
+      # @author = options[:author]
+    end
+
+    # Installation skeleton.  Intermediate directories are automatically
+    # created so don't sweat their absence here.
+    BASEDIRS = %w(
+      Classes
+      test
+    )
+end
---
+++ app_generators/iphoneruby/templates/test/test_helper.rb
@@ -0,0 +1,8 @@
+require "test/unit"
+
+require "rubygems"
+require "Shoulda"
+
+require "osx/cocoa"
+
+$:.unshift File.dirname(__FILE__) + "/../build/bundles"
