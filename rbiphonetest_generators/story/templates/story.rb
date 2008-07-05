1ce2f23f7e0bd76621fef92f54eba10af046aceb
Aaron VonderHaar <gruen0aermel@gmail.com>
2008-07-05 03:02:16 -0700

    Fix typos in the generator templates.

--- rbiphonetest_generators/story/templates/story.rb
+++ rbiphonetest_generators/story/templates/story.rb
@@ -1,5 +1,5 @@
 require File.dirname(__FILE__) + "/story_helper"
 
-with_steps_for(:<%= name =>) do
-  run_local_story "<%= name =>_story"
+with_steps_for(:<%= name %>) do
+  run_local_story "<%= name %>_story"
 end
